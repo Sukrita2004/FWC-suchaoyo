962c89c44b5ba7c0dfacb9e5387a50352c5c097c
Sukrita2004 <Fame2047@gmail.com>
2026-09-17 18:12:56 +0700

    3

--- Discovery_piscine/cell03/ex04/ex00bis/background.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('#btn').on('click', function() {
-        var randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
-        
-        $('body').css('background-color', randomColor);
-    });
-});
